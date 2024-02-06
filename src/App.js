8a079bf6e943cc36aa90e533f35d015ce354ca9e
adhirajgupta <60607630+adhirajgupta@users.noreply.github.com>
2024-02-06 13:18:26 +0530

    changed app.js

--- src/App.js
+++ src/App.js
@@ -35,11 +35,11 @@ const router = createBrowserRouter([
 const App = () => {
   return (
     <RouterProvider router={router}>
-      <MainScreen />
+      {/* <MainScreen />
       <AddItem />
       <ViewMenuScreen />
       <NotFound/>
-      <Portal/>
+      <Portal/> */}
     </RouterProvider>
   );
 };
